a41f610073aa42fa805c2edc50ef47588a70f57a
CocoaHaha <1076800596@qq.com>
2018-07-10 14:28:34 +0800

    Initial Commit

---
+++ 图片擦除/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片擦除
+//
+//  Created by CocoaSXF on 2018/7/10.
+//  Copyright © 2018年 CocoHaHa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 图片擦除/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  图片擦除
+//
+//  Created by CocoaSXF on 2018/7/10.
+//  Copyright © 2018年 CocoHaHa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
