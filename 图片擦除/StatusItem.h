2df75d434ec72581d268ac2972c4d190fe744ecd
CocoaHaha <1076800596@qq.com>
2018-08-07 10:55:38 +0800

    first commit

---
+++ 图片擦除/StatusItem.h
@@ -0,0 +1,13 @@
+//
+//  StatusItem.h
+//  图片擦除
+//
+//  Created by CocoaSXF on 2018/7/20.
+//  Copyright © 2018年 CocoHaHa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StatusItem : NSObject
+
+@end
---
+++ 图片擦除/StatusItem.m
@@ -0,0 +1,13 @@
+//
+//  StatusItem.m
+//  图片擦除
+//
+//  Created by CocoaSXF on 2018/7/20.
+//  Copyright © 2018年 CocoHaHa. All rights reserved.
+//
+
+#import "StatusItem.h"
+
+@implementation StatusItem
+
+@end
